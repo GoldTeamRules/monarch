07a067c1a05438ddc1c437d7987dfa1db19b784f
ricokahler <ricokahler@me.com>
2015-12-12 17:11:43 -0500

    Scaffolded MonitorSightings and ReporterSightings

---
+++ Monarch/Controllers/MonitorSightingsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Monarch.Models.ButterflyTrackingContext;
+
+namespace Monarch.Controllers
+{
+    public class MonitorSightingsController : Controller
+    {
+        private ButterflyTrackingContext db = new ButterflyTrackingContext();
+
+        // GET: MonitorSightings
+        public ActionResult Index()
+        {
+            var machineSightings = db.MachineSightings.Include(m => m.Butterfly).Include(m => m.Monitor).Include(m => m.SightingFileUpload);
+            return View(machineSightings.ToList());
+        }
+
+        // GET: MonitorSightings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonitorSighting monitorSighting = db.MachineSightings.Find(id);
+            if (monitorSighting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(monitorSighting);
+        }
+
+        // GET: MonitorSightings/Create
+        public ActionResult Create()
+        {
+            ViewBag.ButterflyId = new SelectList(db.Butterflies, "ButterflyId", "Name");
+            ViewBag.MonitorId = new SelectList(db.MachineMonitors, "MonitorId", "UniqueName");
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId");
+            return View();
+        }
+
+        // POST: MonitorSightings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MonitorSightingId,MonitorId,ButterflyId,SightingFileUploadId,DateTime,FileUploadId")] MonitorSighting monitorSighting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MachineSightings.Add(monitorSighting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ButterflyId = new SelectList(db.Butterflies, "ButterflyId", "Name", monitorSighting.ButterflyId);
+            ViewBag.MonitorId = new SelectList(db.MachineMonitors, "MonitorId", "UniqueName", monitorSighting.MonitorId);
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId", monitorSighting.SightingFileUploadId);
+            return View(monitorSighting);
+        }
+
+        // GET: MonitorSightings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonitorSighting monitorSighting = db.MachineSightings.Find(id);
+            if (monitorSighting == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ButterflyId = new SelectList(db.Butterflies, "ButterflyId", "Name", monitorSighting.ButterflyId);
+            ViewBag.MonitorId = new SelectList(db.MachineMonitors, "MonitorId", "UniqueName", monitorSighting.MonitorId);
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId", monitorSighting.SightingFileUploadId);
+            return View(monitorSighting);
+        }
+
+        // POST: MonitorSightings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MonitorSightingId,MonitorId,ButterflyId,SightingFileUploadId,DateTime,FileUploadId")] MonitorSighting monitorSighting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(monitorSighting).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ButterflyId = new SelectList(db.Butterflies, "ButterflyId", "Name", monitorSighting.ButterflyId);
+            ViewBag.MonitorId = new SelectList(db.MachineMonitors, "MonitorId", "UniqueName", monitorSighting.MonitorId);
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId", monitorSighting.SightingFileUploadId);
+            return View(monitorSighting);
+        }
+
+        // GET: MonitorSightings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonitorSighting monitorSighting = db.MachineSightings.Find(id);
+            if (monitorSighting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(monitorSighting);
+        }
+
+        // POST: MonitorSightings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MonitorSighting monitorSighting = db.MachineSightings.Find(id);
+            db.MachineSightings.Remove(monitorSighting);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Monarch/Controllers/ReporterSightingsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Monarch.Models.ButterflyTrackingContext;
+
+namespace Monarch.Controllers
+{
+    public class ReporterSightingsController : Controller
+    {
+        private ButterflyTrackingContext db = new ButterflyTrackingContext();
+
+        // GET: ReporterSightings
+        public ActionResult Index()
+        {
+            var humanSightings = db.HumanSightings.Include(r => r.Reporter).Include(r => r.SightingFileUpload);
+            return View(humanSightings.ToList());
+        }
+
+        // GET: ReporterSightings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReporterSighting reporterSighting = db.HumanSightings.Find(id);
+            if (reporterSighting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reporterSighting);
+        }
+
+        // GET: ReporterSightings/Create
+        public ActionResult Create()
+        {
+            ViewBag.ReporterId = new SelectList(db.Reporters, "ReporterId", "Name");
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId");
+            return View();
+        }
+
+        // POST: ReporterSightings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReporterSightingId,ReporterId,SightingFileUploadId,Latitude,Longitude,DateTime,City,StateProvince,Country,PostalCode")] ReporterSighting reporterSighting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HumanSightings.Add(reporterSighting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ReporterId = new SelectList(db.Reporters, "ReporterId", "Name", reporterSighting.ReporterId);
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId", reporterSighting.SightingFileUploadId);
+            return View(reporterSighting);
+        }
+
+        // GET: ReporterSightings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReporterSighting reporterSighting = db.HumanSightings.Find(id);
+            if (reporterSighting == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ReporterId = new SelectList(db.Reporters, "ReporterId", "Name", reporterSighting.ReporterId);
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId", reporterSighting.SightingFileUploadId);
+            return View(reporterSighting);
+        }
+
+        // POST: ReporterSightings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReporterSightingId,ReporterId,SightingFileUploadId,Latitude,Longitude,DateTime,City,StateProvince,Country,PostalCode")] ReporterSighting reporterSighting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reporterSighting).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ReporterId = new SelectList(db.Reporters, "ReporterId", "Name", reporterSighting.ReporterId);
+            ViewBag.SightingFileUploadId = new SelectList(db.SightingFileUploads, "SightingFileUploadId", "SightingFileUploadId", reporterSighting.SightingFileUploadId);
+            return View(reporterSighting);
+        }
+
+        // GET: ReporterSightings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReporterSighting reporterSighting = db.HumanSightings.Find(id);
+            if (reporterSighting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reporterSighting);
+        }
+
+        // POST: ReporterSightings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ReporterSighting reporterSighting = db.HumanSightings.Find(id);
+            db.HumanSightings.Remove(reporterSighting);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
