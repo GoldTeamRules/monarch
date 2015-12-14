267b13f722b29ea26740635812e1c94bf18ff89d
Drewseph94 <drewseph94@yahoo.com>
2015-12-14 14:33:43 -0500

    Added content to "About" page, changed copyright

--- Monarch/Controllers/HomeController.cs
+++ Monarch/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Monarch.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Gold Team Rules!";
 
             return View();
         }
